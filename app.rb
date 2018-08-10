364e1a338e9529e491a7935e0215fa2bbfa0e67f
Ricky Collins <richardcollins52317@gmail.com>
2018-08-09 20:06:21 -0400

    finally got it to push to bucket

--- app.rb
+++ app.rb
@@ -1,18 +1,36 @@
 require 'aws-sdk-s3'
-# require 'sinatra'
 require 'CSV'
+require 'json'
 require_relative 'isbn.rb'
 require_relative 'isbn_csv.rb'
 
 load 'local_ENV.rb' if File.exist?('local_ENV.rb')
 
-# enable :sessions
+s3 = Aws::S3::Client.new(profile: 'MisterAreBe', region: 'us-east-2')
 
-s3 = Aws::S3::Resource.new(region:'us-east-2')
-obj = s3.buckets('rbs-csv-bucket').object_id()
-obj.write(Pathname.new('output_isbn_file.csv'))
+x = s3.get_object(bucket: 'rb-csv-bucket', key: 'input_isbn_file.csv')
+temp = []
+acorn = []
+CSV.parse(x.body, :headers => true) do |row|
+  if isbn_refa3(row[" ISBN"].to_s)
+    temp << "Valid"
+  else
+    temp << "Invalid"
+  end
+  if row[" ISBN"] != nil
+    acorn << row[" ISBN"].to_s
+  else
+    acorn << ''
+  end
+end
+puts "#{temp}"
+puts "#{acorn}"
 
-# get '/' do
-#     erb :index
-# end
+validated = CSV.generate do |csv|
+  csv << [" ISBN", " Is Valid?"]
+  temp.each_with_index do |v,i|
+    csv << [acorn[i], v]
+  end
+end
 
+s3.put_object(bucket: 'rb-csv-bucket', body: validated, key: 'output_isbn_file.csv')
